b23231725a672f9bb627c5c9190276ba3808c7bf
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-09-15 02:07:35 +0200

    footer main done

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function HomePage() {
   return (
-    <div className='h-[100vh] bg-black dark:bg-[#ffffff]'>HomePage</div>
+    <div className='h-[50vh] bg-black dark:bg-[#ffffff]'>HomePage</div>
   );
 }
 
